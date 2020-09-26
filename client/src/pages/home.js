c3907384687b1545e097f96032309a8f781a65df
Alejandro Alonso Garcia <alexalonso04@ufl.edu>
2020-09-26 19:48:22 -0400

    implemented modal toggle in the homepage

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -6,8 +6,22 @@ export default class Home extends Component {
     constructor() {
         super();
         this.state = {
-
+            event_view: false,
+            event_signup:false,
+            selected_event: null
         };
+        this.toogleEventView = this.toogleEventView.bind(this)
+    }
+
+    toogleEventView(event) {
+        this.setState(state => ({
+            ...state, event_view: !state.event_view
+        }));
+    }
+    toogleEventSignUp(event) {
+        this.setState(state => ({
+            ...state, event_signup: !state.event_signup
+        }));
     }
 
     render() {
@@ -16,6 +30,8 @@ export default class Home extends Component {
                 <Card.Group
                 itemsPerRow={3}>
                     <EventCard
+                        signUp={() => this.toogleEventSignUp({})}
+                        viewInfo={() => this.toogleEventView({})}
                         orgName="Red Cross"
                         eventName="Blood Donation"
                         description="An event where you donate blood, que mas tu quieres"
