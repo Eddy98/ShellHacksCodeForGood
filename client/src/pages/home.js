05ab6bfc7e692b701faab086652567dff6912bac
Alejandro Alonso Garcia <alexalonso04@ufl.edu>
2020-09-26 16:24:46 -0400

    added the card group to the home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Card, CardGroup } from 'semantic-ui-react';
+import EventCard from "../components/EventCard"
 
 export default class Home extends Component {
     constructor() {
@@ -10,7 +12,31 @@ export default class Home extends Component {
 
     render() {
         return (
-            <></>
+            <>
+                <Card.Group
+                itemsPerRow={3}>
+                    <EventCard
+                        orgName="Red Cross"
+                        eventName="Blood Donation"
+                        description="An event where you donate blood, que mas tu quieres"
+                    />
+                    <EventCard
+                        orgName="Red Cross"
+                        eventName="Blood Donation"
+                        description="An event where you donate blood, que mas tu quieres"
+                    />
+                    <EventCard
+                        orgName="Red Cross"
+                        eventName="Blood Donation"
+                        description="An event where you donate blood, que mas tu quieres"
+                    />
+                    <EventCard
+                        orgName="Red Cross"
+                        eventName="Blood Donation"
+                        description="An event where you donate blood, que mas tu quieres"
+                    />
+                </Card.Group>
+            </>
         );
     }
 }
