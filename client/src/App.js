314bd31a1057eb13aa961d3638e02e3f8f97061f
Eddy98 <eduardodgraziano@ufl.edu>
2020-09-26 14:23:00 -0400

    firebase started

--- client/src/App.js
+++ client/src/App.js
@@ -8,26 +8,34 @@ import {
 import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
+import * as db from 'firebase'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 5,
+  }
+
+  componentDidMount() {
+    const rootRef = db.firestore().collection("react").doc("react").get().then(doc => {
+      if (doc.exists) {
+          console.log("Document data:", doc.data());
+      } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+      }
+  }).catch(function(error) {
+      console.log("Error getting document:", error);
+  });
+    console.log(rootRef);
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>{this.state.counter}</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const  firebaseConfig = {
+  apiKey: "AIzaSyAAgUtOHGKz2pyRiA8suOqgHmxxC-vyTVY",
+  authDomain: "shellhackscodeforgood.firebaseapp.com",
+  databaseURL: "https://shellhackscodeforgood.firebaseio.com",
+  projectId: "shellhackscodeforgood",
+  storageBucket: "shellhackscodeforgood.appspot.com",
+  messagingSenderId: "798089780581",
+  appId: "1:798089780581:web:22c37dde2f77122445d58c",
+  measurementId: "G-NJJZQLP8P4"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
