beb644494f2dcfa55ef08b2f7694b3b666cbde9f
Eddy98 <eduardodgraziano@ufl.edu>
2020-09-26 15:26:17 -0400

    getusers and add user util

--- client/src/util/user.util.js
+++ client/src/util/user.util.js
@@ -1,7 +1,32 @@
-module.exports ={
+import * as firebase from 'firebase'
 
-    
-    addUser() {
-
-    }
+const firebaseConfig = {
+  apiKey: 'AIzaSyAAgUtOHGKz2pyRiA8suOqgHmxxC-vyTVY',
+  authDomain: 'shellhackscodeforgood.firebaseapp.com',
+  databaseURL: 'https://shellhackscodeforgood.firebaseio.com',
+  projectId: 'shellhackscodeforgood',
+  storageBucket: 'shellhackscodeforgood.appspot.com',
+  messagingSenderId: '798089780581',
+  appId: '1:798089780581:web:22c37dde2f77122445d58c',
+  measurementId: 'G-NJJZQLP8P4',
 }
+
+firebase.initializeApp(firebaseConfig)
+
+const usersCollection = firebase.firestore().collection('user')
+
+const UserUtil = {
+  addUser: (newUser) => {
+    usersCollection.add({ ...newUser })
+  },
+  getUsers: () => {
+    usersCollection.get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            console.log(doc.data());
+        });
+    })
+  }
+}
+
+export default UserUtil
