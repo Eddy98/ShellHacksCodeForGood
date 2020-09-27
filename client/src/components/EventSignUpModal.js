1e2abfb6c97c9eee689fc0aa9f483ea9767a11a6
Alejandro Alonso Garcia <alexalonso04@ufl.edu>
2020-09-27 00:20:52 -0400

    event sign up modal now has the form completed

--- client/src/components/EventSignUpModal.js
+++ client/src/components/EventSignUpModal.js
@@ -4,12 +4,23 @@ import { Button, Card, Modal, Image, Header, Grid, Form, Divider } from "semanti
 export default class EventSignUpModal extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      times: []
+    };
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange = (_, {name, value}) => {
+    this.setState(state=>({...state, [name]: value}))
   }
 
   render() {
     const { isOpen, onClose, event } = this.props;
-
+    const {phone} = this.state
     if (!event) {
       return <></>;
     }
@@ -32,23 +43,55 @@ export default class EventSignUpModal extends Component {
             <Form.Group>
               <Form.Input
                 label="First Name"
+                name="firstName"
                 required
+                onChange={this.onChange}
               />
               <Form.Input
                 label="Last Name"
                 required
+                onChange={this.onChange}
+
               />
             </Form.Group>
             <Form.Group>
               <Form.Input
                 label="Email"
                 required
+                onChange={this.onChange}
               />
               <Form.Input
                 label="Phone Number"
+                name="phone"
+                value={phone}
                 required
+                onChange={(event,{name,value})=>{
+                  if(!isNaN(value)) {
+                    this.setState(state=>(
+                      {...state, 
+                        [name]: value}))
+                  }
+                }}
               />
             </Form.Group>
+            <Divider/>
+            <Header as="h4">Time Slots</Header>
+            <Form.Checkbox
+              inline
+              name=''
+              label='8:00am - 11:00am'
+              onChange={this.onChange}
+            />
+            <Form.Checkbox
+              inline
+              label='11:00am - 12:00pm'
+              onChange={this.onChange}
+            />
+            <Form.Checkbox
+              inline
+              label='12:00pm - 1:00pm'
+              onChange={this.onChange}
+            />
           </Form>
         </Modal.Content>
         <Modal.Actions>
