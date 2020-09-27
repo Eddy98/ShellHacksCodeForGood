fbc085f0b4620a74a2ae57c99bafef3fd920b292
Alejandro Alonso Garcia <alexalonso04@ufl.edu>
2020-09-26 22:08:58 -0400

    event sign up now takes some fields

--- client/src/components/EventSignUpModal.js
+++ client/src/components/EventSignUpModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Card, Modal, Image, Header, Grid } from "semantic-ui-react";
+import { Button, Card, Modal, Image, Header, Grid, Form, Divider } from "semantic-ui-react";
 
 export default class EventSignUpModal extends Component {
   constructor(props) {
@@ -8,30 +8,53 @@ export default class EventSignUpModal extends Component {
   }
 
   render() {
-    const { isOpen, onClose } = this.props;
+    const { isOpen, onClose, event } = this.props;
+
+    if (!event) {
+      return <></>;
+    }
+    const {eventName, orgName, description} = event;
+
     return (
       <Modal 
         open={isOpen} 
         onClose={onClose}
+        size="tiny"
       >
-        <Modal.Header>eventName</Modal.Header>
-        <Modal.Content image>
-          <Header>orgName</Header>
+        <Modal.Header>{eventName}</Modal.Header>
+        <Modal.Content>
           <Modal.Description>
-            <Grid>
-              <Grid.Row>
-                <Grid.Column>
-                  <p>description</p>
-                </Grid.Column>
-                <Grid.Column>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
+            <Header as="h5">{orgName}</Header>
+            <p>{description}</p>
           </Modal.Description>
+          <Divider/>
+          <Form>
+            <Form.Group>
+              <Form.Input
+                label="First Name"
+                required
+              />
+              <Form.Input
+                label="Last Name"
+                required
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Input
+                label="Email"
+                required
+              />
+              <Form.Input
+                label="Phone Number"
+                required
+              />
+            </Form.Group>
+          </Form>
         </Modal.Content>
         <Modal.Actions>
-          <Button>Cancel</Button>
-          <Button positive>Ok</Button>
+          <Button
+            onClick={onClose}>Cancel</Button>
+          <Button positive>Sign-up</Button>
         </Modal.Actions>
       </Modal>
     );
